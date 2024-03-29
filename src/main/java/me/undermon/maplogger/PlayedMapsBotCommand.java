ad84633b696c7fd6b6e5a4465b3bec55a8ad5409
Undermon <undermon@duck.com>
2024-03-29 01:58:16 -0300

    Renamed and moved discord command class.

--- src/main/java/me/undermon/maplogger/PlayedMapsBotCommand.java
+++ src/main/java/me/undermon/maplogger/PlayedMapsBotCommand.java
@@ -1,4 +1,7 @@
-package me.undermon.maplogger.discord;
+package me.undermon.maplogger;
+
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
 
 import javax.sql.DataSource;
 
@@ -7,6 +10,8 @@ import org.javacord.api.event.interaction.AutocompleteCreateEvent;
 import org.javacord.api.event.interaction.SlashCommandCreateEvent;
 import org.javacord.api.interaction.DiscordLocale;
 import org.javacord.api.interaction.SlashCommandBuilder;
+import org.javacord.api.interaction.SlashCommandInteraction;
+import org.javacord.api.interaction.SlashCommandInteractionOption;
 import org.javacord.api.interaction.SlashCommandOption;
 import org.javacord.api.interaction.SlashCommandOptionBuilder;
 import org.javacord.api.interaction.SlashCommandOptionChoice;
@@ -15,25 +20,61 @@ import org.javacord.api.interaction.SlashCommandOptionType;
 import org.javacord.api.listener.interaction.AutocompleteCreateListener;
 import org.javacord.api.listener.interaction.SlashCommandCreateListener;
 
-import me.undermon.maplogger.ConfigFile;
+public class PlayedMapsBotCommand implements SlashCommandCreateListener, AutocompleteCreateListener {
+	private static final String TIME_OPTION = "time";
+	private static final String UNIT_OPTION = "unit";
+	private static final String SERVER_OPTION = "server";
 
-public class PlayedCommand implements SlashCommandCreateListener, AutocompleteCreateListener {
-	private static final String NAME = "played";
+	private static final String COMMAND_NAME = "played";
+	private static final Duration MAX_SEARCH = Duration.of(180, ChronoUnit.DAYS);
 
-	private ConfigFile configFile;
-	private DataSource dataSource;
-
-	public PlayedCommand(ConfigFile configFile, DataSource dataSource) {
+	private final ConfigFile configFile;
+	private final DataSource dataSource;
+	
+	public PlayedMapsBotCommand(ConfigFile configFile, DataSource dataSource) {
 		this.configFile = configFile;
 		this.dataSource = dataSource;
 	}
 
 	@Override
 	public void onSlashCommandCreate(SlashCommandCreateEvent event) {
-		if (event.getSlashCommandInteraction().getFullCommandName().equals(NAME)) {
-			event.getSlashCommandInteraction().createImmediateResponder().setContent(Thread.currentThread().getName()).respond();
-
+		SlashCommandInteraction command = event.getSlashCommandInteraction();
+	
+		if (!command.getFullCommandName().equals(COMMAND_NAME)) {
+			return;
 		}
+	
+		long time = Math.abs(command.getOptionByName(TIME_OPTION).flatMap(t -> t.getLongValue()).orElse(1L));
+		
+		ChronoUnit unit = command.getOptionByName(UNIT_OPTION).
+			flatMap(SlashCommandInteractionOption::getStringValue).
+			map(ChronoUnit::valueOf).
+			orElse(ChronoUnit.DAYS);
+		
+		String serverIdentifier = command.getOptionByName(SERVER_OPTION).
+			flatMap(SlashCommandInteractionOption::getStringValue).
+			orElse("");
+
+		Duration search = Duration.of(time, unit);
+
+
+		if (search.compareTo(MAX_SEARCH) > 0) {
+			String message;
+
+			if (command.getLocale() == DiscordLocale.PORTUGUESE_BRAZILIAN) {
+				message = "Você pode listar o histórico de mapas de até seis meses atrás.";
+			} else {
+				message = "You can list map history up to six months ago.";
+			}
+
+			command.createImmediateResponder().setContent(message).respond();
+		}
+
+
+		event.getSlashCommandInteraction().createImmediateResponder().setContent(
+			time + " / " + unit + " / " + serverIdentifier
+		).respond();
+
 	}
 
 	@Override
@@ -45,14 +86,13 @@ public class PlayedCommand implements SlashCommandCreateListener, AutocompleteCr
 		);
 	}
 
-	public static void register(DiscordApi api) {
-		
+	static void register(DiscordApi api) {
 
 		SlashCommandOption time = new SlashCommandOptionBuilder().
 			setType(SlashCommandOptionType.LONG).
 			setLongMaxValue(180).
 			setMinLength(1).
-			setName("time").
+			setName(TIME_OPTION).
 			setDescription("List the last played maps in this amount of time.").
 			addNameLocalization(DiscordLocale.PORTUGUESE_BRAZILIAN, "tempo").
 			addDescriptionLocalization(DiscordLocale.PORTUGUESE_BRAZILIAN, "Lista os últimos mapas jogadados neste período de tempo.").
@@ -61,20 +101,20 @@ public class PlayedCommand implements SlashCommandCreateListener, AutocompleteCr
 		SlashCommandOptionChoice hour = new SlashCommandOptionChoiceBuilder().
 			setName("hours").
 			addNameLocalization(DiscordLocale.PORTUGUESE_BRAZILIAN, "horas").
-			setValue("hour").
+			setValue(ChronoUnit.HOURS.name()).
 			build();
 
 		SlashCommandOptionChoice day = new SlashCommandOptionChoiceBuilder().
 			setName("days"). 
 			addNameLocalization(DiscordLocale.PORTUGUESE_BRAZILIAN, "dias").
-			setValue("day").
+			setValue(ChronoUnit.DAYS.name()).
 			build();
 
 		SlashCommandOption timeUnit = new SlashCommandOptionBuilder().
 			setType(SlashCommandOptionType.STRING).
 			addChoice(hour).
 			addChoice(day).
-			setName("unit").
+			setName(UNIT_OPTION).
 			setDescription("Time unit to be used for the search.").
 			addNameLocalization(DiscordLocale.PORTUGUESE_BRAZILIAN, "unidade").
 			addDescriptionLocalization(DiscordLocale.PORTUGUESE_BRAZILIAN, "Unidade de tempo a ser usada na busca.").
@@ -83,16 +123,15 @@ public class PlayedCommand implements SlashCommandCreateListener, AutocompleteCr
 		SlashCommandOption server = new SlashCommandOptionBuilder().
 			setType(SlashCommandOptionType.STRING).
 			setRequired(false).
-			setName("server").
+			setName(SERVER_OPTION).
 			setDescription("List the played maps on this server").
 			addNameLocalization(DiscordLocale.PORTUGUESE_BRAZILIAN, "servidor").
 			addDescriptionLocalization(DiscordLocale.PORTUGUESE_BRAZILIAN, "Lista os mapas jogados neste servidor").
 			setAutocompletable(true).
 			build();
 
-	
 		new SlashCommandBuilder().
-			setName(NAME).
+			setName(COMMAND_NAME).
 			setDescription("View the map history of a server.").
 			addNameLocalization(DiscordLocale.PORTUGUESE_BRAZILIAN, "jogados").
 			addDescriptionLocalization(DiscordLocale.PORTUGUESE_BRAZILIAN, "Veja histórico de mapas de um servidor.").
